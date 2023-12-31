16c423602f6d38fbc8f9d9a1b1e947980e8e9a16
Vanish <70620798+IAmVan1sh@users.noreply.github.com>
2023-12-30 18:41:33 -0700

    Changing api url to the current value

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {AuthResponse} from "../types/AuthResponse";
 
-export const API_URL = 'https://friendly-wrap-boa.cyclic.app/api';
+export const API_URL = 'https://cute-pink-bull-cuff.cyclic.app/api';
 
 const $api = axios.create({
 	withCredentials: true,
